a2210c38bc896af724972c024c2f98aae96c5767
lukadd16 <lukadd@icloud.com>
2023-03-16 16:50:30 -0400

    chore: fixed Tiled paths

---
+++ run_away/resources/gfx/tilesets/terrain_snow.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="terrain_snow.tsx" tilewidth="16" tileheight="16" tilecount="280" columns="14">
+ <image source="../terrain/terrain_snow.png" width="224" height="320"/>
+</tileset>
--- run_away/resources/gfx/tilesets/terrain_winter.tsx
+++
@@ -1,4 +0,0 @@
-<?xml version="1.0" encoding="UTF-8"?>
-<tileset version="1.10" tiledversion="1.10.0" name="terrain_winter.tsx" tilewidth="16" tileheight="16" tilecount="280" columns="14">
- <image source="../../../../../Assets/fourSeasonsPlatformer_ [tileset, version 2.0]/midground_/winter_.png" width="224" height="320"/>
-</tileset>

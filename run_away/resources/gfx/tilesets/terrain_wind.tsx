86ad94917495899ceabae73bc08d1ec4f128a023
KelseyKloosterman <kkloost2@uwo.ca>
2023-03-17 07:55:43 -0400

    Added some new tilesets for levels from the terrain pictures, and created a second hub level.

---
+++ run_away/resources/gfx/tilesets/terrain_wind.tsx
@@ -0,0 +1,4 @@
+<?xml version="1.0" encoding="UTF-8"?>
+<tileset version="1.10" tiledversion="1.10.0" name="terrain_wind" tilewidth="16" tileheight="16" tilecount="220" columns="11">
+ <image source="../terrain/terrain_wind.png" width="176" height="320"/>
+</tileset>
